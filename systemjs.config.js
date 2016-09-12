301843889a568bc32fd64dc93de435791e97165f
HungNguyen <hungnguyen@users-iMac.local>
2016-09-12 11:48:51 +0700

    Refactor @angular bundles, remove 'format' in @angular2-material bundles

--- systemjs.config.js
+++ systemjs.config.js
@@ -13,20 +13,13 @@
       // our app is within the app folder
       app: 'app',
 
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+			// angular
+			'@angular':										'npm:@angular',
+      '@angular2-material':         'npm:@angular2-material',
 
       // other libraries
       'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
-      '@angular2-material':         'npm:@angular2-material'
+      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -34,26 +27,35 @@
         main: './main.js',
         defaultExtension: 'js'
       },
+			// angular bundles:
+			'@angular/core': { main: 'bundles/core.umd.js' },
+			'@angular/common': { main: 'bundles/common.umd.js' },
+			'@angular/compiler': { main: 'bundles/compiler.umd.js' },
+			'@angular/platform-browser': { main: 'bundles/platform-browser.umd.js' },
+			'@angular/platform-browser-dynamic': { main: 'bundles/platform-browser-dynamic.umd.js' },
+			'@angular/http': { main: 'bundles/http.umd.js' },
+			'@angular/router': { main: 'bundles/router.umd.js' },
+			'@angular/forms': { main: 'bundles/forms.umd.js' },
       // angular2-material bundles
-      '@angular2-material/button': { main: 'button.umd.js', format: 'cjs' },
-      '@angular2-material/button-toggle': { main: 'button-toggle.umd.js', format: 'cjs' },
-      '@angular2-material/card': { main: 'card.umd.js', format: 'cjs' },
-      '@angular2-material/core': { main: 'core.umd.js', format: 'cjs' },
-      '@angular2-material/checkbox': { main: 'checkbox.umd.js', format: 'cjs' },
-      '@angular2-material/grid-list': { main: 'grid-list.umd.js', format: 'cjs' },
-      '@angular2-material/icon': { main: 'icon.umd.js', format: 'cjs' },
-      '@angular2-material/input': { main: 'input.umd.js', format: 'cjs' },
-      '@angular2-material/list': { main: 'list.umd.js', format: 'cjs' },
-      '@angular2-material/menu': { main: 'menu.umd.js', format: 'cjs' },
-      '@angular2-material/progress-bar': { main: 'progress-bar.umd.js', format: 'cjs' },
-      '@angular2-material/progress-circle': { main: 'progress-circle.umd.js', format: 'cjs' },
-      '@angular2-material/radio': { main: 'radio.umd.js', format: 'cjs' },
-      '@angular2-material/sidenav': { main: 'sidenav.umd.js', format: 'cjs' },
-      '@angular2-material/slide-toggle': { main: 'slide-toggle.umd.js', format: 'cjs' },
-      '@angular2-material/slider': { main: 'slider.umd.js', format: 'cjs' },
-      '@angular2-material/tabs': { main: 'tabs.umd.js', format: 'cjs' },
-      '@angular2-material/toolbar': { main: 'toolbar.umd.js', format: 'cjs' },
-      '@angular2-material/tooltip': { main: 'tooltip.umd.js', format: 'cjs' },
+      '@angular2-material/button': { main: 'button.umd.js' },
+      '@angular2-material/button-toggle': { main: 'button-toggle.umd.js' },
+      '@angular2-material/card': { main: 'card.umd.js' },
+      '@angular2-material/core': { main: 'core.umd.js' },
+      '@angular2-material/checkbox': { main: 'checkbox.umd.js' },
+      '@angular2-material/grid-list': { main: 'grid-list.umd.js' },
+      '@angular2-material/icon': { main: 'icon.umd.js' },
+      '@angular2-material/input': { main: 'input.umd.js' },
+      '@angular2-material/list': { main: 'list.umd.js' },
+      '@angular2-material/menu': { main: 'menu.umd.js' },
+      '@angular2-material/progress-bar': { main: 'progress-bar.umd.js' },
+      '@angular2-material/progress-circle': { main: 'progress-circle.umd.js' },
+      '@angular2-material/radio': { main: 'radio.umd.js' },
+      '@angular2-material/sidenav': { main: 'sidenav.umd.js' },
+      '@angular2-material/slide-toggle': { main: 'slide-toggle.umd.js' },
+      '@angular2-material/slider': { main: 'slider.umd.js' },
+      '@angular2-material/tabs': { main: 'tabs.umd.js' },
+      '@angular2-material/toolbar': { main: 'toolbar.umd.js' },
+      '@angular2-material/tooltip': { main: 'tooltip.umd.js' },
       rxjs: {
         defaultExtension: 'js'
       },
